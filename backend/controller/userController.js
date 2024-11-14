dfde0daf1d15f27b8d34c330d515c092b73f1e78
prantik2003 <guinprantik@gmail.com>
2024-11-14 20:14:53 +0530

    updated

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -201,6 +201,8 @@ export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
     .cookie("adminToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
@@ -215,6 +217,8 @@ export const logoutPatient = catchAsyncErrors(async (req, res, next) => {
     .cookie("patientToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,7 +1,7 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   // Determine the cookie name based on the user's role
-  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
+  const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
 
   res
     .status(statusCode)
@@ -10,6 +10,8 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
@@ -18,4 +20,3 @@ export const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
